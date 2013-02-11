5f1f41832effe3ec94306ac0f2931d58a91c1888
Jason Smith (work) <jhs@iriscouch.com>
2013-02-11 07:47:44 +0000

    Remove unused code

--- src/couchdb/couch_httpd_debug.erl
+++ src/couchdb/couch_httpd_debug.erl
@@ -116,49 +116,6 @@ request_to_iolist(#httpd{method=Method, mochi_req=MochiReq}) -> ok
     , [Action, "\r\n", Headers, "\r\n"]
     .
 
-%% Login handler with Browser ID.
-%handle_id_req(#httpd{method='POST'}=Req) -> ok
-%    , case couch_config:get("browserid", "enabled")
-%        of "true" -> ok
-%            , case couch_config:get("browserid", "audience", undefined)
-%                of undefined -> ok
-%                    , throw({error, no_browserid_audience})
-%                ; Audience -> ok
-%                    , handle_id_req(enabled, Audience, Req)
-%                end
-%        ; _ -> ok
-%            % Browserid is disabled in the config.
-%            , throw({error, browserid_not_enabled})
-%        end
-%    ;
-%
-%handle_id_req(_Req) ->
-%    % Send 405
-%    not_implemented.
-%
-%handle_id_req(enabled, Audience, #httpd{method='POST', mochi_req=MochiReq}=Req) ->
-%    ReqBody = MochiReq:recv_body(),
-%    Form = case MochiReq:get_primary_header_value("content-type") of
-%        % content type should be json
-%        "application/x-www-form-urlencoded" ++ _ ->
-%            mochiweb_util:parse_qs(ReqBody);
-%        "application/json" ++ _ ->
-%            {Pairs} = ?JSON_DECODE(ReqBody),
-%            lists:map(fun({Key, Value}) ->
-%              {?b2l(Key), ?b2l(Value)}
-%            end, Pairs);
-%        _ ->
-%            []%couch_httpd:send_json(Req, 406, {error, method_not_allowed})
-%    end,
-%    Assertion = couch_util:get_value("assertion", Form, ""),
-%    case verify_id(Assertion, Audience) of
-%    {error, _Reason} ->
-%        % Send client an error response, couch_util:send_err ...
-%        not_implemented;
-%    {ok, Verified_obj} -> ok
-%        , send_good_id(Req, Verified_obj)
-%    end.
-
 
 %
 % Utilities
